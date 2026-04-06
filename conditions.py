0efa2c121a275b131d1e36ead064ceeb26c854da
Pravin P Patil <prvnpatil11@gmail.com>
2026-04-06 11:54:58 +0530

    conditions

---
+++ conditions.py
@@ -0,0 +1,18 @@
+no = 20
+
+if no >= 30:
+    print(f"{no} greater than 30");
+else:
+    print(f"{no} lesser than 30");
+
+if no <= 30:
+    print(f"{no} greater than 30");
+else:
+    print(f"{no} lesser than 30");
+
+if no >= 30:
+    print(f"{no} greater than 30");
+elif no ==20:
+    print(f"{no} equals to 20");
+else:
+    print(f"{no} else");
