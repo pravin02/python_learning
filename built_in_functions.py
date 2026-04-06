cf9a9316b4f6f0a352bfc42e34354b32129da819
Pravin P Patil <prvnpatil11@gmail.com>
2026-04-06 10:49:09 +0530

    variables

---
+++ built_in_functions.py
@@ -0,0 +1,8 @@
+name = "Pravin Patil";
+print(len(name), " is the lenth of the employee name ", name);
+
+print("First character of string ", name[0]);
+print("Last character of string ", name[-1]);
+print("First three characters of string ", name[0:3]);
+print("All the characters of string ", name[0:]);
+print("All the characters of string ", name[:]);
---
+++ variables.py
@@ -0,0 +1,9 @@
+employee_name="Pravin Patil";
+employee_count = 1000;
+average_salary = 30000.00;
+is_active= True;
+
+print("Employee Name ", employee_name);
+print("Total employees are ",employee_count);
+print("Average salary of an employee is ",average_salary);
+print("Is Active ", is_active);
