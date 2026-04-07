5c1f19527749289369b5db7d0fe97ce8a04e9cdc
Pravin P Patil <prvnpatil11@gmail.com>
2026-04-07 21:05:25 +0530

    if elif

--- conditions.py
+++ conditions.py
@@ -16,3 +16,24 @@ elif no ==20:
     print(f"{no} equals to 20");
 else:
     print(f"{no} else");
+
+
+light = "Green";
+
+if light == "Green":
+    print("Go");
+elif light == "Orange":
+    print("Caution");
+elif light == "Red":
+    print("Stop");
+else:
+    print("Incorrect light singal");
+
+
+users = ["Pravin", "Rahul"];
+if users:
+    print("Users is not empty");
+
+users = [];
+if not users:
+    print("Users is empty");
