4a30bf8296d07b7386c8856a5fe98ee9c0a2e545
Pravin Patil <prvnpatil11@gmail.com>
2026-04-12 17:41:42 +0530

    multiple exception in single except block

--- exception_hadling.py
+++ exception_hadling.py
@@ -1,6 +1,8 @@
 # This program demonstrates how to handle exceptions in python
 
-print("This program takes inputs from user converts them into integers and devides first number with second number")
+print(
+    "This program takes inputs from user converts them into integers and devides first number with second number"
+)
 
 try:
     no1 = input("Enter First Number: ")
@@ -15,12 +17,12 @@ except ValueError:
     print("Excption: Inputs must be integer number's only")
 except ZeroDivisionError as e:
     print(f"Exception Occured: {e}")
- 
+
 
 print("\n Raise exception example")
 try:
     no1 = int(input("Enter First Number: "))
-    no2 = int(input("Enter Second Number"))
+    no2 = int(input("Enter Second Number: "))
 
     if no2 == 0:
         raise RuntimeError("Number is 0")
@@ -33,3 +35,17 @@ except ValueError as e:
     print(f"Value Error: please enter intergers only. e: {e}")
 except ZeroDivisionError:
     print("No2 must not be zero")
+
+
+print("\n Catch multiple exceptions in except block example")
+try:
+    no1 = int(input("Enter First Number: "))
+    no2 = int(input("Enter Second Number: "))
+
+    if no2 == 0:
+        raise RuntimeError("Number is 0")
+    div = no1 + no2
+    print(f"{no1}/{no2}={div}")
+
+except (RuntimeError, ValueError, ZeroDivisionError) as e:
+    print(f"Runtime Exception: {e}")
