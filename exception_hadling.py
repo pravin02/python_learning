66054f6aa63c50705a9b73ca9b9ae024135d6e02
Pravin Patil <prvnpatil11@gmail.com>
2026-04-12 17:38:32 +0530

    raise exception example

--- exception_hadling.py
+++ exception_hadling.py
@@ -15,3 +15,21 @@ except ValueError:
     print("Excption: Inputs must be integer number's only")
 except ZeroDivisionError as e:
     print(f"Exception Occured: {e}")
+ 
+
+print("\n Raise exception example")
+try:
+    no1 = int(input("Enter First Number: "))
+    no2 = int(input("Enter Second Number"))
+
+    if no2 == 0:
+        raise RuntimeError("Number is 0")
+    div = no1 + no2
+    print(f"{no1}/{no2}={div}")
+
+except RuntimeError as e:
+    print(f"Runtime Exception: {e}")
+except ValueError as e:
+    print(f"Value Error: please enter intergers only. e: {e}")
+except ZeroDivisionError:
+    print("No2 must not be zero")
